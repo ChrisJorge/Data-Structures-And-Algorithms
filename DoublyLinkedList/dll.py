d82349d0ea3dd262b29b15befdd22b812325c3f3
Christopher Jorge <Christopherjorge47@icloud.com>
2024-07-07 17:51:34 -0400

    Added node and doublyLinkedList class

---
+++ DoublyLinkedList/dll.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value = None):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.length = 0
+        self.head = None
+        self.tail = None
